42b5e5e7be9f345b1d80d6c1a24c172602e27cd8
Gil Adam <gil@giladam.com>
2014-10-28 19:05:08 -0700

    added tests for ip-like patterns

---
+++ src/test/java/com/giladam/listmatch/ListMatcherIpPatternsTest.java
@@ -0,0 +1,46 @@
+package com.giladam.listmatch;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Sets;
+
+
+public class ListMatcherIpPatternsTest {
+
+
+
+    @Test
+    public void testIpPatternHandling() {
+
+        Collection<String> patterns = Sets.newHashSet("10.0.0.*",
+                                                      "10.100.0.1");
+
+        PatternList patternList = new PatternList(patterns, ".", false);
+
+        String[] expectedMatches = {"10.0.0.1",
+                                    "10.0.0.100",
+                                    "10.100.0.1"};
+
+        String[] expectedNotMatches = {"20.0.0.2",
+                                       "20.0.0.200",
+                                       "20.100.0.1",
+                                       "10.100.0.2",
+                                       "10.100.1.0"};
+
+        for (String expectedMatch : expectedMatches) {
+            Assert.assertTrue("Expected " + expectedMatch + " to match.",
+                              patternList.matches(expectedMatch));
+        }
+
+
+        for (String expectedNotMatch : expectedNotMatches) {
+            Assert.assertFalse("Expected " + expectedNotMatch + " to NOT match.",
+                               patternList.matches(expectedNotMatch));
+        }
+    }
+
+
+}
